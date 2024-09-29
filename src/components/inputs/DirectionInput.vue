17f595b140e364bde9a80cb7b3d08f761c387282
KaoSDlanor <KaoSDlanor@gmail.com>
2024-09-29 14:45:55 +0200

    Removed angle input component because it doesn't scale well

--- src/components/inputs/DirectionInput.vue
+++ src/components/inputs/DirectionInput.vue
@@ -1,47 +1,26 @@
 <template>
-	<div class="DirectionInput__container">
-		<PrimeInputNumber
-			ref="primeInputNumber"
-			v-model="modelValue"
-			@input="modelValue = $event.value"
-			suffix="°"
-			locale="en-UK"
-			:allowEmpty="false"
-			highlightOnFocus
-			:minFractionDigits="0"
-			:maxFractionDigits="1"
-		/>
-		<AngleInput
-			class="Wind__information__item__angle-input"
-			:value="wrapDegrees(-modelValue - viewport.rotation + 180)"
-			@change="modelValue = wrapDegrees(-$event - viewport.rotation + 180)"
-			:step="1"
-		/>
-	</div>
+	<PrimeInputNumber
+		ref="primeInputNumber"
+		v-model="modelValue"
+		@input="modelValue = $event.value"
+		suffix="°"
+		locale="en-UK"
+		:allowEmpty="false"
+		highlightOnFocus
+		:minFractionDigits="0"
+		:maxFractionDigits="1"
+	/>
 </template>
-
-<style lang="scss">
-	.DirectionInput__container {
-		display: flex;
-		flex-direction: row;
-		gap: 1rem;
-	}
-</style>
 
 <script setup lang="ts">
 	import PrimeInputNumber from 'primevue/inputnumber';
 	import { defineModel, nextTick, onMounted, ref } from 'vue';
-	import AngleInput from 'vue-angle-input';
-	import { injectViewport } from '@/contexts/viewport';
-	import { wrapDegrees } from '@/lib/angle';
 
 	const primeInputNumber = ref<InstanceType<typeof PrimeInputNumber>>(null!);
 
 	const props = defineProps<{
 		autoFocus?: boolean;
 	}>();
-
-	const viewport = injectViewport();
 
 	const modelValue = defineModel({ type: Number, required: true });
 
