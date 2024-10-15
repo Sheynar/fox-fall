3e07ca6fc6ab18b59d5664b6544f5959c7f51eda
KaoSDlanor <KaoSDlanor@gmail.com>
2024-10-15 16:16:35 +0200

    Base units are now draggable by default

--- packages/ui/src/components/Unit/Unit.vue
+++ packages/ui/src/components/Unit/Unit.vue
@@ -185,7 +185,7 @@
 	const unitLabel = computed(() => getUnitLabel(unitMap.value, unit.value.id));
 
 	const isHovered = ref(false);
-	const canDrag = ref(settings.value.showMap && unit.value.parentId == null);
+	const canDrag = ref(unit.value.parentId == null);
 	const open = computed({
 		get: () => selectedUnits.value.includes(unit.value.id),
 		set: (value) => {
