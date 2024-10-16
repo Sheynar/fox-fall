3d6d271edae1eb79c07abb46932eb155abb99c43
KaoSDlanor <KaoSDlanor@gmail.com>
2024-10-16 09:38:41 +0200

    Slight tweak to unit icon positioning

--- packages/ui/src/components/Unit/Unit.vue
+++ packages/ui/src/components/Unit/Unit.vue
@@ -76,7 +76,7 @@
 		top: calc(var(--unit-y) * 1px);
 		z-index: 1000;
 
-		transform: translate(-50%, -100%) scale(var(--unit-icon-scale)) translateY(-20.73%);
+		transform: translate(-50%, -100%) scale(var(--unit-icon-scale)) translateY(-21%);
 		transform-origin: 50% 100%;
 
 		&:hover,
