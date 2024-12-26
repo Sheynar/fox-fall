c6afb642f686443bc0b2f348ad0992288cfa8d53
KaoSDlanor <KaoSDlanor@gmail.com>
2024-12-26 15:32:58 +0200

    Added a basic context menu when the rotation of the viewport is locked

--- packages/ui/src/App.vue
+++ packages/ui/src/App.vue
@@ -10,11 +10,17 @@
 		@contextmenu.prevent="onContextMenu"
 		tabindex="-1"
 	>
-		<Grid />
+		<Grid @contextMenu="onContextMenu" />
 
 		<Viewport />
 
 		<OverlayHud @calibrate-grid="() => calibrateGrid()" />
+
+		<ContextMenu
+			ref="contextMenu"
+			:model="contextMenuOptions"
+			@hide="() => (contextMenuPosition = null)"
+		/>
 
 		<svg>
 			<defs>
@@ -67,22 +73,26 @@
 </style>
 
 <script setup lang="ts">
-	import { computed, ref } from 'vue';
+	import ContextMenu from 'primevue/contextmenu';
+	import type { MenuItem } from 'primevue/menuitem';
+	import { computed, nextTick, onMounted, ref, watch } from 'vue';
 	import Grid from '@/components/Grid.vue';
 	import OverlayHud from '@/components/OverlayHud/OverlayHud.vue';
 	import Viewport from '@/components/Viewport/Viewport.vue';
 	import { artillery } from '@/lib/globals';
 	import { settings } from '@/lib/settings';
+	import { getUnitResolvedVector, UnitType } from '@/lib/unit';
 	import { Vector } from '@/lib/vector';
 	import { useUnitGroup } from '@/mixins/unit-group';
 	import { useViewPortControl } from '@/mixins/viewport-control';
 
 	const containerElement = ref<null | HTMLDivElement>(null);
+	const contextMenu = ref<null | InstanceType<typeof ContextMenu>>(null);
 
 	const unitGroupIds = computed(() => Object.keys(artillery.unitMap.value));
 	const unitGroup = useUnitGroup(artillery.unitMap, unitGroupIds);
 
-	const { calibrating, calibrateGrid } = useViewPortControl({
+	const { calibrating, calibrateGrid, canRotate } = useViewPortControl({
 		containerElement,
 		viewport: artillery.viewport,
 		lockPan: computed(() => {
@@ -106,13 +116,78 @@
 		};
 	};
 
+	const contextMenuPosition = ref<Vector | null>(null);
 	const onContextMenu = (event: MouseEvent) => {
 		if (artillery.unitSelector.value != null) {
 			event.preventDefault();
 			event.stopPropagation();
 			artillery.unitSelector.value.selectUnit(null);
+		} else if (!canRotate.value) {
+			contextMenuPosition.value = artillery.cursor.value.clone();
+			contextMenu.value?.show(event);
 		}
 	};
+	const contextMenuOptions = computed(() => {
+		const output: MenuItem[] = [
+			{
+				label: artillery.selectedUnits.value.length > 0 ? 'Add base unit' : 'Add unit',
+				items: [
+					UnitType.Artillery,
+					UnitType.Spotter,
+					UnitType.Location,
+					UnitType.Target,
+					UnitType.LandingZone,
+				].map((unitType) => ({
+					label: `${UnitType[unitType]}`,
+					command: () => {
+						artillery.addUnit(
+							unitType,
+							undefined,
+							ref(
+								artillery.viewport.value.toWorldPosition(
+									contextMenuPosition.value!
+								)
+							)
+						);
+					},
+				})),
+			},
+		];
+
+		if (artillery.selectedUnits.value.length > 0) {
+			output.push({
+				label: 'Add relative unit',
+				items: [
+					UnitType.Artillery,
+					UnitType.Spotter,
+					UnitType.Location,
+					UnitType.Target,
+					UnitType.LandingZone,
+				].map((unitType) => ({
+					label: `${UnitType[unitType]}`,
+					command: () => {
+						artillery.addUnit(
+							unitType,
+							undefined,
+							ref(
+								artillery.viewport.value
+									.toWorldPosition(contextMenuPosition.value!)
+									.addVector(
+										getUnitResolvedVector(
+											artillery.unitMap.value,
+											artillery.selectedUnits.value[0]
+										).scale(-1)
+									)
+							),
+							artillery.selectedUnits.value[0]
+						);
+					},
+				})),
+			});
+		}
+
+		return output;
+	});
 
 	const isTransparent =
 		new URL(location.href).searchParams.get('overlay') != null;
