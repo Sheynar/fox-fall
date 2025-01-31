7e7eca1f395a9f133d49cf2ed1014019a8a5616d
KaoSDlanor <KaoSDlanor@gmail.com>
2025-01-31 12:44:34 +0200

    Updated settings modal so it uses tabs to separate the settings

--- packages/ui/src/components/OverlayHud/Settings.vue
+++ packages/ui/src/components/OverlayHud/Settings.vue
@@ -7,146 +7,207 @@
 		@pointerdown.stop
 		@wheel.stop
 	>
-		<div class="Settings__content">
-			<div class="Settings__row">
-				<label>Show tooltip</label>
-				<PrimeCheckBox binary v-model="settings.showTooltip" @update:model-value="saveSettings" />
-			</div>
-			<div class="Settings__row">
-				<label>Use NATO alphabet</label>
-				<PrimeCheckBox binary v-model="settings.useNatoAlphabet" @update:model-value="saveSettings" />
-			</div>
-			<div class="Settings__row">
-				<label>Show X/Y offsets</label>
-				<PrimeCheckBox binary v-model="settings.showXYOffsets" @update:model-value="saveSettings" />
-			</div>
-			<div class="Settings__row">
-				<label>Grid dash length</label>
-				<PrimeInputNumber
-					:model-value="settings.gridDashLength"
-					@input="settings.gridDashLength = Number($event.value); saveSettings()"
-					suffix="px"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Grid dash gap</label>
-				<PrimeInputNumber
-					:model-value="settings.gridDashGap"
-					@input="settings.gridDashGap = Number($event.value); saveSettings()"
-					suffix="px"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Grid line width</label>
-				<PrimeInputNumber
-					:model-value="settings.gridLineWidth"
-					@input="settings.gridLineWidth = Number($event.value); saveSettings()"
-					suffix="px"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0.25"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Compass opacity</label>
-				<PrimeInputNumber
-					:model-value="Math.floor(settings.compassOpacity * 100)"
-					@input="settings.compassOpacity = Math.floor(Number($event.value)) / 100; saveSettings()"
-					suffix="%"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="0"
-					:min="0"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Firing arc opacity</label>
-				<PrimeInputNumber
-					:model-value="Math.floor(settings.firingArcOpacity * 100)"
-					@input="settings.firingArcOpacity = Math.floor(Number($event.value)) / 100; saveSettings()"
-					suffix="%"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="0"
-					:min="0"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Screenshot opacity</label>
-				<PrimeInputNumber
-					:model-value="Math.floor(settings.screenshotOpacity * 100)"
-					@input="settings.screenshotOpacity = Math.floor(Number($event.value)) / 100; saveSettings()"
-					suffix="%"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="0"
-					:min="0.1"
-				/>
-			</div>
-			<div class="Settings__row" v-if="isOverlay">
-				<label>Overlay button scale</label>
-				<PrimeInputNumber
-					:model-value="settings.toggleButtonScale"
-					@input="settings.toggleButtonScale = Number($event.value); saveSettings()"
-					suffix="x"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0.25"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Unit icon scale</label>
-				<PrimeInputNumber
-					:model-value="settings.unitIconScale"
-					@input="settings.unitIconScale = Number($event.value); saveSettings()"
-					suffix="x"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0.25"
-				/>
-			</div>
-			<div class="Settings__row">
-				<label>Unit settings scale</label>
-				<PrimeInputNumber
-					:model-value="settings.unitSettingsScale"
-					@input="settings.unitSettingsScale = Number($event.value); saveSettings()"
-					suffix="x"
-					locale="en-UK"
-					:allowEmpty="false"
-					highlightOnFocus
-					:minFractionDigits="0"
-					:maxFractionDigits="2"
-					:min="0.25"
-				/>
-			</div>
-		</div>
+		<Tabs value="features">
+			<TabList>
+				<Tab value="features">Feature selection</Tab>
+				<Tab value="grid">Grid tweaks</Tab>
+				<Tab value="interface">Interface tweaks</Tab>
+			</TabList>
+			<TabPanels>
+				<TabPanel value="features">
+					<div class="Settings__content">
+						<div class="Settings__row">
+							<label>Show tooltip</label>
+							<PrimeCheckBox
+								binary
+								v-model="settings.showTooltip"
+								@update:model-value="saveSettings"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Use NATO alphabet</label>
+							<PrimeCheckBox
+								binary
+								v-model="settings.useNatoAlphabet"
+								@update:model-value="saveSettings"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Show X/Y offsets</label>
+							<PrimeCheckBox
+								binary
+								v-model="settings.showXYOffsets"
+								@update:model-value="saveSettings"
+							/>
+						</div>
+					</div>
+				</TabPanel>
+				<TabPanel value="grid">
+					<div class="Settings__content">
+						<div class="Settings__row">
+							<label>Grid dash length</label>
+							<PrimeInputNumber
+								:model-value="settings.gridDashLength"
+								@input="
+									settings.gridDashLength = Number($event.value);
+									saveSettings();
+								"
+								suffix="px"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Grid dash gap</label>
+							<PrimeInputNumber
+								:model-value="settings.gridDashGap"
+								@input="
+									settings.gridDashGap = Number($event.value);
+									saveSettings();
+								"
+								suffix="px"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Grid line width</label>
+							<PrimeInputNumber
+								:model-value="settings.gridLineWidth"
+								@input="
+									settings.gridLineWidth = Number($event.value);
+									saveSettings();
+								"
+								suffix="px"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0.25"
+							/>
+						</div>
+					</div>
+				</TabPanel>
+				<TabPanel value="interface">
+					<div class="Settings__content">
+						<div class="Settings__row">
+							<label>Compass opacity</label>
+							<PrimeInputNumber
+								:model-value="Math.floor(settings.compassOpacity * 100)"
+								@input="
+									settings.compassOpacity =
+										Math.floor(Number($event.value)) / 100;
+									saveSettings();
+								"
+								suffix="%"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="0"
+								:min="0"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Firing arc opacity</label>
+							<PrimeInputNumber
+								:model-value="Math.floor(settings.firingArcOpacity * 100)"
+								@input="
+									settings.firingArcOpacity =
+										Math.floor(Number($event.value)) / 100;
+									saveSettings();
+								"
+								suffix="%"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="0"
+								:min="0"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Screenshot opacity</label>
+							<PrimeInputNumber
+								:model-value="Math.floor(settings.screenshotOpacity * 100)"
+								@input="
+									settings.screenshotOpacity =
+										Math.floor(Number($event.value)) / 100;
+									saveSettings();
+								"
+								suffix="%"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="0"
+								:min="0.1"
+							/>
+						</div>
+						<div class="Settings__row" v-if="isOverlay">
+							<label>Overlay button scale</label>
+							<PrimeInputNumber
+								:model-value="settings.toggleButtonScale"
+								@input="
+									settings.toggleButtonScale = Number($event.value);
+									saveSettings();
+								"
+								suffix="x"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0.25"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Unit icon scale</label>
+							<PrimeInputNumber
+								:model-value="settings.unitIconScale"
+								@input="
+									settings.unitIconScale = Number($event.value);
+									saveSettings();
+								"
+								suffix="x"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0.25"
+							/>
+						</div>
+						<div class="Settings__row">
+							<label>Unit settings scale</label>
+							<PrimeInputNumber
+								:model-value="settings.unitSettingsScale"
+								@input="
+									settings.unitSettingsScale = Number($event.value);
+									saveSettings();
+								"
+								suffix="x"
+								locale="en-UK"
+								:allowEmpty="false"
+								highlightOnFocus
+								:minFractionDigits="0"
+								:maxFractionDigits="2"
+								:min="0.25"
+							/>
+						</div>
+					</div>
+				</TabPanel>
+			</TabPanels>
+		</Tabs>
 	</PrimeDialog>
 </template>
 
@@ -169,6 +230,11 @@
 	import PrimeDialog from 'primevue/dialog';
 	import PrimeCheckBox from 'primevue/checkbox';
 	import PrimeInputNumber from 'primevue/inputnumber';
+	import Tab from 'primevue/tab';
+	import Tabs from 'primevue/tabs';
+	import TabList from 'primevue/tablist';
+	import TabPanel from 'primevue/tabpanel';
+	import TabPanels from 'primevue/tabpanels';
 	import { isOverlay } from '@/lib/constants';
 	import { saveSettings, settings } from '@/lib/settings';
 
