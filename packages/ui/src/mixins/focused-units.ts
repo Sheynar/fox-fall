6a29dbac53e2dc69e99f9f21cf519b7e3c38398d
KaoSDlanor <KaoSDlanor@gmail.com>
2025-03-07 14:02:03 +0200

    Improved the order of the focused units list

--- packages/ui/src/mixins/focused-units.ts
+++ packages/ui/src/mixins/focused-units.ts
@@ -4,19 +4,21 @@ import { computed } from 'vue';
 
 export const useFocusedUnitIds = () => {
 	return computed(() => {
-		const focusedUnitsSet = new Set([
+		const focusedUnits = [
 			...artillery.highlightedUnits.value,
-			...artillery.pinnedUnits.value,
-		]);
+		];
 
-		if (artillery.selectedUnit.value != null)
-			focusedUnitsSet.add(artillery.selectedUnit.value);
-
-		const artilleryUnitIds = Object.keys(artillery.unitMap.value).filter((unitId) => artillery.unitMap.value[unitId].type === UnitType.Artillery);
-		if (artilleryUnitIds.length === 1 && !focusedUnitsSet.has(artilleryUnitIds[0])) {
-			focusedUnitsSet.add(artilleryUnitIds[0]);
+		if (artillery.selectedUnit.value != null) {
+			focusedUnits.push(artillery.selectedUnit.value);
 		}
 
-		return Array.from(focusedUnitsSet);
+		focusedUnits.push(...artillery.pinnedUnits.value);
+
+		const artilleryUnitIds = Object.keys(artillery.unitMap.value).filter((unitId) => artillery.unitMap.value[unitId].type === UnitType.Artillery);
+		if (artilleryUnitIds.length === 1 && !focusedUnits.includes(artilleryUnitIds[0])) {
+			focusedUnits.push(artilleryUnitIds[0]);
+		}
+
+		return Array.from(new Set(focusedUnits));
 	});
 };
