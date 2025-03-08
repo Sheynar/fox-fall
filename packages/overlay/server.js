029164668c583aa3bc7e17e7c0a483583f01ec24
KaoSDlanor <KaoSDlanor@gmail.com>
2025-03-08 14:54:52 +0200

    Added more logging for the integrated webserver

--- packages/overlay/server.js
+++ packages/overlay/server.js
@@ -3,4 +3,12 @@
  * when we configure typescript to use esnext it replaces the async import with a require() call
  * require() fails because @packages/server uses ESM syntax
  */
-module.exports = import('@packages/server/dist/ws.js').then((mod) => mod.initialiseWebsocket());
+module.exports = import("@packages/server/dist/ws.js")
+	.then((mod) => mod.initialiseWebsocket())
+	.then(() => {
+		console.log('Sync server ready');
+	})
+	.catch((err) => {
+		console.log("Failed to set up sync server");
+		console.error(err);
+	});
