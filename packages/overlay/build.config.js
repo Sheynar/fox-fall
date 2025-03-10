39ac31c6bad67ac0edc1eac3bb6ceba554c442c8
KaoSDlanor <KaoSDlanor@gmail.com>
2025-03-10 10:48:49 +0200

    Updated build config to eleminate space characters in the setup file name

--- packages/overlay/build.config.js
+++ packages/overlay/build.config.js
@@ -4,7 +4,7 @@ require('electron-builder');
 module.exports = {
 	appId: "co.za.fox-fall.overlay",
 	productName: "FoxFall Overlay",
-	artifactName: "${productName}-${version}-${os}-${arch}.${ext}",
+	artifactName: "FoxFall-${version}-${os}-${arch}.${ext}",
 	npmRebuild: false,
 	directories: {
 		output: "out/${productName}/${os}",
