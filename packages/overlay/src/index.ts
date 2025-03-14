6a95a3555877ed1591dced52d42c20a1f2095d1c
KaoSDlanor <KaoSDlanor@gmail.com>
2025-03-14 20:28:30 +0200

    Re-disabled the app menu

--- packages/overlay/src/index.ts
+++ packages/overlay/src/index.ts
@@ -1,11 +1,11 @@
-import { app, BrowserWindow, Menu } from 'electron';
-import '../server.js';
+import { app, BrowserWindow, Menu } from "electron";
+import "../server.js";
 
 app.on("window-all-closed", () => {
 	if (process.platform !== "darwin") app.quit();
 });
 
-// Menu.setApplicationMenu(null)
+Menu.setApplicationMenu(null);
 
 const initialise = async () => {
 	await app.whenReady();
