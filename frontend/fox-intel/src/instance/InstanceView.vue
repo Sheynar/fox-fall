43e6dcd6b87e86038d3540f062a52ca26e63b287
KaoSDlanor <KaoSDlanor@gmail.com>
2026-01-11 21:10:24 +0200

    Removed some old unneeded styling

--- frontend/fox-intel/src/instance/InstanceView.vue
+++ frontend/fox-intel/src/instance/InstanceView.vue
@@ -139,45 +139,11 @@
 		cursor: initial;
 
 		color: var(--color-primary);
+		background-color: var(--color-primary-contrast);
 		outline: none;
 
 		will-change: transform;
 		transform: translateZ(0);
-
-		&:not(.App__transparent) {
-			background-color: var(--color-primary-contrast);
-		}
-
-		&.App__hidden {
-			filter: opacity(0);
-			pointer-events: none;
-		}
-
-		&.App__screenshot {
-			cursor: none;
-		}
-
-		&.App__calibrating,
-		&.App__screenshot {
-			opacity: 0;
-			pointer-events: none;
-		}
-	}
-
-	.App__hexMapCanvas {
-		pointer-events: none;
-	}
-
-	.App__markerCanvas {
-		pointer-events: initial;
-	}
-
-	.App__hexMapCanvas,
-	.App__markerCanvas {
-		position: fixed;
-		inset: 0;
-		width: 100dvw;
-		height: 100dvh;
 	}
 </style>
 
