fcf57c7f5b72ad6f14c8b67e49ed6d92d40d235e
KaoSDlanor <KaoSDlanor@gmail.com>
2026-02-03 14:20:28 +0200

    Added a bit of an animation to the landing page patreon modal

--- frontend/fox-intel/src/LandingPage.vue
+++ frontend/fox-intel/src/LandingPage.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="LandingPage__container">
+	<div class="LandingPage__container" :class="{ 'LandingPage__container-hidden': dismissPatreonModal }">
 		<h1 class="LandingPage__title">FoxIntel</h1>
 		<div class="LandingPage__body">
 			<p class="LandingPage__description">
@@ -19,15 +19,34 @@
 		</div>
 	</div>
 
-	<div class="LandingPage__pateon-modal" v-if="dismissPatreonModal">
-		<p>Please consider <a target="_blank"
-				href="https://www.patreon.com/cw/KaoSDlanor?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink">supporting
-				me on Patreon</a>. I will soon need to rent a larger server to handle hosting the application</p>
-		<button @click="dismissPatreonModal()">Just sign me in already</button>
+	<div class="LandingPage__patreon-modal-backdrop" v-if="dismissPatreonModal" @click="dismissPatreonModal = null">
+		<div class="LandingPage__patreon-modal">
+			<div class="LandingPage__patreon-modal-inner" @click.stop>
+				<p>Please consider <a target="_blank"
+						href="https://www.patreon.com/cw/KaoSDlanor?utm_medium=unknown&utm_source=join_link&utm_campaign=creatorshare_creator&utm_content=copyLink">supporting
+						me on Patreon</a>. I will soon need to rent a larger server to handle hosting the application</p>
+				<button @click="dismissPatreonModal()">Just sign me in already</button>
+			</div>
+		</div>
 	</div>
 </template>
 
 <style lang="scss">
+
+@keyframes modal-enter {
+	0% {
+		transform-origin: center center;
+		transform: scale(0);
+		opacity: 0;
+	}
+
+	100% {
+		transform-origin: center center;
+		transform: scale(1);
+		opacity: 1;
+	}
+}
+
 .LandingPage__container {
 	height: 100%;
 	width: 100%;
@@ -36,6 +55,18 @@
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
+
+
+	transform-origin: center center;
+	transform: scale(1);
+	opacity: 1;
+	transition: transform 0.25s ease-in-out, opacity 0.25s ease-in-out;
+
+	&.LandingPage__container-hidden {
+		transform: scale(1.5);
+		opacity: 0;
+		pointer-events: none;
+	}
 }
 
 .LandingPage__title {
@@ -54,8 +85,20 @@
 	font-size: 0.8em;
 }
 
-.LandingPage__pateon-modal {
+.LandingPage__patreon-modal-backdrop {
 	position: fixed;
+	inset: 0;
+	background-color: rgba(0, 0, 0, 0.5);
+}
+
+.LandingPage__patreon-modal {
+	position: absolute;
+	inset: 0;
+	animation: modal-enter .25s ease-in-out forwards;
+}
+
+.LandingPage__patreon-modal-inner {
+	position: absolute;
 	top: 50%;
 	left: 50%;
 	transform: translate(-50%, -50%);
